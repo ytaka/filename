bf698ae8568b956267c28b4cd722d44abe9d9b3d
Takayuki YAMAGUCHI <d@ytak.info>
2011-03-25 15:04:39 +0900

    Add main file and spec file.

---
+++ lib/filename.rb
@@ -0,0 +1,183 @@
+autoload :FileUtils, 'fileutils'
+
+# 
+# The class to create filename that is not duplicated.
+# We select type of additional part of filename: number or time.
+# 
+class FileName
+
+  # The options are following:
+  # * :start
+  #     Fixnum
+  #     If ID string type is number, the ID starts from the specified number.
+  # 
+  # * :digit
+  #     Fixnum
+  #     When we create additional part of a filename,
+  #     we use a string of ID number with specified digit.
+  # 
+  # * :delimiter
+  #     String
+  #     We use specified string for delimiter between base name and
+  #     additional part. Default is '.' if position is suffix. Otherwise, '_'.
+  # 
+  # * :type
+  #     :number or :time
+  #     We specify type of additional part: :number or :time.
+  #     Default is :number.
+  # 
+  # * :format
+  #     String or Proc
+  #     We specify format string of additional part or
+  #     proc object to create additional part.
+  #     If type is :time, the format string is used by Time#strftime.
+  #     For :number type, the string is a farst argument of sprintf(format, number).
+  #     Proc object takes an object of Time or Integer for respective types.
+  # 
+  # * :position
+  #     :prefix, :suffix, or :middle
+  #     We specify of position of additional part of filename.
+  def initialize(basepath, opts = {})
+    @basepath = File.expand_path(basepath)
+    @number = opts[:start] || 0
+    @digit = opts[:digit] || 2
+    @type = opts[:type] || :number
+    @position = opts[:position] || :suffix
+    @delimiter = opts[:delimiter] || (@position == :suffix ? '.' : '_')
+    @format = opts[:format]
+    @last_addition = nil
+  end
+
+  def get_basepath(extension = nil)
+    if extension
+      extension = '.' + extension unless extension[0] == '.'
+      oldext = File.extname(@basepath)
+      if oldext.size > 0
+        @basepath.sub(Regexp.new("\\#{oldext}$"), extension)
+      else
+        @basepath + extension
+      end
+    else
+      @basepath
+    end
+  end
+  private :get_basepath
+
+  def create_time_addition(t)
+    case @format
+    when String
+      t.strftime(@format)
+    when Proc
+      @format.call(t)
+    else
+      t.strftime("%Y%m%d_%H%M%S_") + sprintf("%06d", t.usec)
+    end
+  end
+  private :create_time_addition
+
+  def create_number_addition(number)
+    case @format
+    when String
+      sprintf(@format, number)
+    when Proc
+      @format.call(number)
+    else
+      sprintf("%0#{@digit}d", number)  
+    end
+  end
+  private :create_number_addition
+
+  def get_addition(add, filename)
+    if add != :prohibit && (add == :always || File.exist?(filename))
+      case @type
+      when :time
+        s = create_time_addition(Time.now)
+      when :number
+        s = create_number_addition(@number)
+        @number += 1
+      else
+        raise "Invalid type of addition."
+      end
+      if s.size == 0 || s.include?(' ') || s.include?('/')
+        raise "Invalid additional part of filename: #{s.inspect}"
+      elsif @last_addition == s
+        raise "Create same addition: #{s.inspect}"
+      end
+      @last_addition = s
+      return s
+    end
+    nil
+  end
+  private :get_addition
+
+  def add_addition(filename, addition)
+    case @position
+    when :prefix
+      dir, base = File.split(filename)
+      dir + '/' + addition + @delimiter + base
+    when :middle
+      dir, base = File.split(filename)
+      ext = File.extname(base)
+      if ext.size > 0
+        filename.sub(Regexp.new("\\#{ext}$"), @delimiter + addition + ext)
+      else
+        filename + @delimiter + addition
+      end
+    else # :suffix
+      filename + @delimiter + addition
+    end
+  end
+  private :add_addition
+
+  # The options are following:
+  # * :extension
+  #     String of extension
+  #     If we want to change extension, we set the value of the option.
+  # 
+  # * :add
+  #     :always, :auto, or :prohibit
+  #     We specify if the additional part is used.
+  #       :always    Always add.
+  #       :auto      If specified file exists, add.
+  #       :prohibit  Even if specified file exists, not add.
+  # 
+  # * :directory
+  #     true or false
+  #     If the value is true and the parent directory does not exist,
+  #     we create the directory.
+  def create(opts = {})
+    base = get_basepath(opts[:extension])
+    FileUtils.mkdir_p(File.dirname(base)) if opts[:directory]
+    if addition = get_addition(opts[:add], base)
+      path = add_addition(base, addition)
+      while File.exist?(path)
+        if addition = get_addition(opts[:add], base)
+          path = add_addition(base, addition)
+        else
+          raise "Can not create new filename."
+        end
+      end
+      path
+    else
+      base
+    end
+  end
+
+  # Executing FileName.new and FileName.create, we get new filename.
+  # The options for both FileName.new and FileName#create are available.
+  def self.create(basepath, opts = {})
+    opts_new = {}
+    opts_create = {}
+    opts.each do |key, val|
+      case key
+      when :extension, :add, :directory
+        opts_create[key] = val
+      else
+        opts_new[key] = val
+      end
+    end
+    fname = self.new(basepath, opts_new)
+    fname.create(opts_create)
+  end
+
+end
--- spec/filename_spec.rb
+++ spec/filename_spec.rb
@@ -1,7 +1,114 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Filename" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe FileName do
+  it "should return unchanged filename" do
+    filename = FileName.new("abc.txt")
+    filename.create.should == File.expand_path(File.dirname('.') + '/abc.txt')
   end
+
+  it "should return new filename with number" do
+    filename = FileName.new(__FILE__)
+    path = filename.create
+    File.exist?(path).should_not be_true
+    path.should match(/\.\d+$/)
+  end
+
+  it "should return new filename with number with digit" do
+    filename = FileName.new(__FILE__, :digit => 3, :position => :suffix)
+    filename.create.should match(/\.000$/)
+    filename.create.should match(/\.001$/)
+  end
+
+  it "should return new filename with specified number" do
+    filename = FileName.new(__FILE__, :digit => 3, :position => :suffix, :start => 10)
+    filename.create.should match(/\.010$/)
+    filename.create.should match(/\.011$/)
+  end
+
+  it "should return new filename with specified delimiter" do
+    filename = FileName.new(__FILE__, :digit => 2, :position => :suffix, :delimiter => '@')
+    filename.create.should match(/@00$/)
+  end
+
+  it "should return new filename from format string of sprintf" do
+    filename = FileName.new(__FILE__, :format => "ABC%dDEF")
+    filename.create.should match(/ABC0DEF$/)
+  end
+
+  it "should return new filename from proc object" do
+    filename = FileName.new(__FILE__, :start => 3, :format => lambda { |n| sprintf("%03d", n * n) })
+    filename.create.should match(/\.009$/)
+  end
+
+  it "should raise error" do
+    filename = FileName.new(__FILE__, :format => "")
+    lambda do
+      filename.create
+    end.should raise_error
+  end
+
+  it "should return new filename with time" do
+    filename = FileName.new(__FILE__, :type => :time)
+    path = filename.create
+    File.exist?(path).should_not be_true
+    path.should match(/\.[\d_]+$/)
+  end
+
+  it "should return new filename from format string of time" do
+    filename = FileName.new(__FILE__, :type => :time, :format => "%Y")
+    filename.create.should match(/\.\d{4}$/)
+    lambda do
+      filename.create
+    end.should raise_error
+  end
+
+  it "should return new filename from proc object" do
+    filename = FileName.new(__FILE__, :type => :time, :format => lambda { |t| t.strftime("%Y_%Y") })
+    filename.create.should match(/\.\d\d\d\d_\d\d\d\d$/)
+  end
+
+  it "should return same filename" do
+    filename = FileName.new(__FILE__)
+    path = filename.create(:add => :prohibit)
+    path.should == File.expand_path(__FILE__)
+  end
+
+  it "should return same filename" do
+    file = File.join(__FILE__, 'not_exist')
+    filename = FileName.new(file)
+    path = filename.create
+    path.should == file
+  end
+
+  it "should return filename with additional part" do
+    file = File.join(__FILE__, 'not_exist')
+    filename = FileName.new(file)
+    path = filename.create(:add => :always)
+    path.should_not == file
+  end
+
+  it "should return filename with prefix" do
+    filename = FileName.new(__FILE__, :position => :prefix)
+    path = filename.create
+    dir, name = File.split(path)
+    name.should match(/^\d+_/)
+  end
+
+  it "should return filename with addition before extension" do
+    filename = FileName.new(__FILE__, :position => :middle)
+    path = filename.create
+    dir, name = File.split(path)
+    ext = File.extname(name)
+    name.should match(Regexp.new("_\\d+\\#{ext}"))
+  end
+
+  it "should create parent directory" do
+    basename = File.join(File.dirname(__FILE__), 'abc/def')
+    filename = FileName.new(basename)
+    path = filename.create(:directory => true)
+    dir = File.dirname(basename)
+    File.exist?(dir).should be_true
+    Dir.rmdir(dir)
+  end
+
 end
